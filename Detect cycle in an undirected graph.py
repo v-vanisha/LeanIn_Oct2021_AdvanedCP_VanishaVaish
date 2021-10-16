d36de932268d12fa4c60a38e855aab542692ec7e
Vanisha Vaish <vanisha.vaish@gmail.com>
2021-10-16 19:47:40 +0530

    Create Detect cycle in an undirected graph.py

---
+++ Detect cycle in an undirected graph.py
@@ -0,0 +1,19 @@
+def isCycle(self, V, adj):
+		vis = [0]*V
+		
+		def dfs(cur, parent):
+		    vis[cur] = 1
+		    
+		    for i in adj[cur]:
+		        if not vis[i]:
+		            if dfs(i, cur): return True
+		        elif i != parent:
+		            return True
+		            
+		    return False
+		
+		for i in range(V):
+		    if not vis[i]:
+		        if dfs(i, -1): return True
+		        
+		return False
